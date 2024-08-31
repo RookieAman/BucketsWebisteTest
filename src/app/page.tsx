907c8d059e18b5617c6b2caa9a020d8494eb1604
RookieAman <rookieaman@gmail.com>
2024-08-31 02:20:41 +0200

    Button animation v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,19 +20,19 @@ export default function Home() {
       <h1 className="text-3xl md:text-5xl xl:text-8xl font-champBlack w-full max-w-[600px] xl:max-w-[900px] text-center mb-8 md:mb-12 lg:mb-16 xl:mb-20">
         Out of this world chocolate
       </h1>
-      {/* <a
+      <a
         href="mailto:contact@bucketschocolate.com?subject=Hello%20Buckets!&body=Hello%20Buckets,%0D%0A%0D%0AI%20would%20like%20to%20get%20more%20information%20about%20your%20services.%0D%0A%0D%0AThank%20you%20in%20advance,"
-        className="bg-secondary text-darkchoco rounded-full px-10 md:px-14 xl:px-20 py-2 xl:py-5 uppercase text-2xl md:text-3xl xl:text-4xl font-montserrat font-extrabold mb-2 xl:mb-5"
+        className="transition-all duration-200 bg-secondary hover:bg-darkchoco hover:text-secondary border-2 border-secondary border-solid text-darkchoco rounded-full px-10 md:px-14 xl:px-20 py-2 xl:py-5 uppercase text-2xl md:text-3xl xl:text-4xl font-montserrat font-extrabold mb-2 xl:mb-5"
       >
         Contact
-      </a> */}
-      <a
+      </a>
+      {/* <a
         href="mailto:contact@bucketschocolate.com?subject=Hello%20Buckets!&body=Hello%20Buckets,%0D%0A%0D%0AI%20would%20like%20to%20get%20more%20information%20about%20your%20services.%0D%0A%0D%0AThank%20you%20in%20advance,"
         className="px-10 md:px-14 xl:px-20 py-2 xl:py-5 uppercase text-2xl relative md:text-3xl xl:text-4xl font-montserrat font-extrabold mb-2 xl:mb-5 rounded-full group overflow-hidden bg-secondary text-darkchoco inline-block border-2 border-solid border-secondary"
       >
         <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-darkchoco group-hover:h-full"></span>
         <span className="relative group-hover:text-secondary">Contact</span>
-      </a>
+      </a> */}
       <p className="text-center font-montserrat text-sm md:text-xl xl:text-2xl text-white max-w-[350px] md:max-w-[400px] xl:max-w-[600px]">
         or write us at{' '}
         <span className="font-bold">contact@bucketschocolate.com</span> for more
